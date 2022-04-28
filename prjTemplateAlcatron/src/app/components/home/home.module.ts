40024839ace0455c68a8a03c2f8e6e232081e40c
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2022-04-28 01:52:45 +0000

    Criado o subcomponente 'cards-info', no qual será responsável por exibir os cards informacionais em relação ao conteúdo do site na página inicial Home

--- prjTemplateAlcatron/src/app/components/home/home.module.ts
+++ prjTemplateAlcatron/src/app/components/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { DestaqueCarrosselComponent } from './destaque-carrossel/destaque-carrossel.component';
+import { CardsInfoComponent } from './cards-info/cards-info.component';
 
 
 
 @NgModule({
   declarations: [
     HomeComponent,
-    DestaqueCarrosselComponent
+    DestaqueCarrosselComponent,
+    CardsInfoComponent
   ],
   imports: [
     CommonModule
