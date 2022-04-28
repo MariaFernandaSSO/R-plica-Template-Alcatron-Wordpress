78f2bd64c02d714e0c02eed95960d10be47cd7b7
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2022-04-28 01:49:48 +0000

    Criado o componente footer

--- prjTemplateAlcatron/src/app/app.module.ts
+++ prjTemplateAlcatron/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './components/topo/topo.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
