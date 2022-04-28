9e19ba11544b6c9c5b4866031d1c490392f3a63f
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2022-04-28 22:53:17 +0000

    Finalizado o subcomponente cards-info - também foram utilizados vetores neste para exibir o texto e os icons

--- prjTemplateAlcatron/src/app/components/home/cards-info/cards-info.component.ts
+++ prjTemplateAlcatron/src/app/components/home/cards-info/cards-info.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsInfoComponent implements OnInit {
 
+  cards = [
+    {
+      icon : '../../../../assets/svgs/emoji-laughing-fill.svg',
+      text : 'Excepcional Serviço'
+    },
+    {
+      icon : '../../../../assets/svgs/lightbulb-fill.svg',
+      text : 'Incrível Design'
+    },
+    {
+      icon : '../../../../assets/svgs/display (1).svg',
+      text : 'Totalmente Responsive'
+    },
+    {
+      icon : '../../../../assets/svgs/gear-fill.svg',
+      text : 'Fácil Personalização'
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
