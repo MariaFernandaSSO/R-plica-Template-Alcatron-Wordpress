6529e3805b16b2d68e6363fc45d9a7900eff9397
Maria Fernanda Oliveira <mariafernandasso281@gmail.com>
2022-04-28 21:53:30 +0000

    Finalizado o subcomponente da home 'destaque-carrossel'. Onde foi utilizado vetores pra exibir as imagens.

--- prjTemplateAlcatron/src/app/components/home/destaque-carrossel/destaque-carrossel.component.ts
+++ prjTemplateAlcatron/src/app/components/home/destaque-carrossel/destaque-carrossel.component.ts
@@ -7,6 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DestaqueCarrosselComponent implements OnInit {
 
+  public imagem = [
+  {
+    mostrar:"active",
+    imagens: [
+      {
+        img: '../../../../assets/img/borboleta1.jpg',
+        text1 : 'Totalmente sensível',
+        text2 : 'Muitas shortcodes',
+        text3 : 'Construido na base 4'
+      }
+    ]
+  },
+  {
+    mostrar: '',
+    imagens: 
+    [
+      {
+      img: '../../../../assets/img/borboleta2.webp',
+      text1 : 'Um site bonito',
+      text2 : 'Admiravel...',
+      text3 : 'Apenas imagine'
+      }
+    ]
+  },
+  {
+    mostrar: '',
+    imagens:
+    [
+      {
+      img: '../../../../assets/img/borboleta3.jpg',
+      text1 : 'Construa conosco',
+      text2 : 'Imenso Brilho',
+      text3 : 'Venha explorar'
+      }
+    ]
+  }
+]
+
+
   constructor() { }
 
   ngOnInit(): void {
